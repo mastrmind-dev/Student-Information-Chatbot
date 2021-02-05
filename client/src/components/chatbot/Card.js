28271d43b43af177716f62f305105540fbde962a
mastrmind-dev <asapthaka@gmail.com>
2021-02-05 14:46:12 +0530

    give fixed width and height to those cards(now it doesn't change on the resolution of images)

--- client/src/components/chatbot/Card.js
+++ client/src/components/chatbot/Card.js
@@ -3,12 +3,12 @@ import React from "react";
 const Card = (props) => {
 	return (
 		<div style={{ width: 270, paddingRight: 30 }}>
-			<div className="card ">
-				<div className="card-image" style={{width:240}}>
+			<div className="card">
+				<div className="card-image" style={{width:240, height: 240, overflow:"hidden"}}>
 					<img src={props.payload.fields.image.stringValue} alt={props.payload.fields.header.stringValue} />
 					<span className="card-title">{props.payload.fields.header.stringValue}</span>
 				</div>
-				<div className="card-content">
+				<div className="card-content" style={{width:240, height: 100, overflow:"hidden"}}>
 					{props.payload.fields.description.stringValue}
                     <p><a>{props.payload.fields.price.stringValue}</a></p>
 				</div>
